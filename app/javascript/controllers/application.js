129ebea07801b5167b29fe505d05f411a19d6506
David Colby <davidlcolby@gmail.com>
2022-02-03 14:56:05 -0600

    Initial commit with esbuild, Tailwind, and SR

---
+++ app/javascript/controllers/application.js
@@ -0,0 +1,13 @@
+import { Application } from "@hotwired/stimulus"
+import StimulusReflex from 'stimulus_reflex'
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.warnings = true
+application.debug    = false
+window.Stimulus      = application
+
+StimulusReflex.initialize(application, { isolate: true })
+
+export { application }
